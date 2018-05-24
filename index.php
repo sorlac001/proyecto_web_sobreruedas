b62d03d59c2a8cd9e93d0f8db1b12e1359419b25
dannGarcam <soy.donny@gmail.com>
2018-05-23 23:31:57 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="HearColors - UNAM">
+    <link rel="icon" href="../../favicon.ico">
+    <link rel="stylesheet" href="common/css/main.css"/>
+
+    <title>Inicio | SobreRuedas</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="common/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font awesome -->
+    <link href="common/css/fa-svg-with-js.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="common/css/main.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Script de FontAwesome -->
+    <script src="common/js/fontawesome-all.min.js"></script>
+  </head>
+
+<body>
+
+<div class="container cont-white">
+  <div class="row contenedor-superior">
+    <div class="col-md-6">
+      <img class="img-responsive img-logo" src="img/logo.png">
+    </div>
+    <div class="col-md-6 text-right contenedor-letras">
+      
+      <div class="btn-group" role="group" aria-label="...">
+        <button class="btn btn-letra btn_aumento_letra" type="button" aria-live="assertive" aria-describedby="desc_letra" role="alert" aria-label="Aumento de letra">A+</button>
+        <button class="btn btn-letra btn_decremento_letra" type="button" aria-live="assertive" aria-describedby="desc_letra" role="alert" aria-label="Decremento de letra">A-</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<nav class="navbar ">
+  <div class="container navbar-iap">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Menú</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav navbar-right navbar-menu-iap">
+        <li>
+          <a href="#organizacion">LA ORGANIZACIÓN</a>
+        </li>
+        <li>
+          <a href="#deportes">deportes</a>
+        </li>
+        <li>
+          <a href="#alianzas">ALIANZAS</a>
+        </li>
+        <li>
+          <a href="#donar">¿CÓMO AYUDAR?</a>
+        </li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
+
+
+
+
+<!-- Begin page content -->
+<section class="contenido-principal">
+  <div class="container cont-white">
+
+    <div class="row slider">
+      <div class="contenedor-carrusel">
+        <img src="img/1.jpg" class="img-carrusel element_carrusel_1 carrusel-active" alt="img 1">
+        <img src="img/3.jpg" class="img-carrusel element_carrusel_2" alt="img 2">
+        <img src="img/4.jpg" class="img-carrusel element_carrusel_3" alt="img 3">
+        <img src="img/2.jpg" class="img-carrusel element_carrusel_4" alt=" img 4">
+      </div>
+
+      <div class="col-md-12 text-center contenedor-btn-carrusel">
+
+        <div class="btn-group" role="group" aria-label="Controles del carrusel de imagenes">
+          <button type="button" class="btn btn-carrusel control_carrusel" data-target="pausa">Pausar</button>
+          <button type="button" class="btn btn-carrusel control_carrusel_1 active" data-target="1">1</button>
+          <button type="button" class="btn btn-carrusel control_carrusel_2" data-target="2">2</button>
+          <button type="button" class="btn btn-carrusel control_carrusel_3" data-target="3">3</button>
+          <button type="button" class="btn btn-carrusel control_carrusel_4" data-target="4">4</button>
+        </div>
+
+      </div>
+
+    </div>
+    <!-- LA ORGANIZACION -->
+    <div id="organizacion" class="row contenedor-organizacion">
+      <div class="col-md-7 col-md-offset-4">
+        <div class="page-header">
+          <h1>¿Quienes somos?</h1>
+        </div>
+      </div>
+      <div class="col-md-4 text-center">
+        <img src="./img/logo_min.png" class="img-lateral" alt="">
+      </div>
+      <div class="col-md-7">
+        
+        <p>
+          "SobreRuedas" es una empresa mexicana amante de los deportes extremos y comprometida con traer a nuestros clientes productos de la más alta calidad que los acompañen en sus aventuras día con día, por que sabemos que sobre ruedas no hay límites.
+          <br>
+          <br>
+          Su fundación respondió a la necesidad de disponer de deportes de apoyo a la educación y rehabilitación de las personas con
+          discapacidad visual en la Ciudad de México. Sus primeros deportes fueron la Escuela Secundaria No. 51 “Antonio Vicente
+          Mosquete” y el Centro de Atención Múltiple No. 50 que ofrecían atención educativa a estudiantes con discapacidad visual, así
+          también fuimos cofundadores de la Escuela para Entrenamiento de Perros Guía de México.
+          <br>
+          <br>
+          Nuestra organización fue creada por un visionario grupo de personas comprometidas con el desarrollo de las personas con
+          discapacidad visual encabezadas por Ignacio León Robles Robles quién fue, entre otras cosas: administrador de empresas,
+          matemático, docente, escritor, activista, líder social, promotor del respeto de los derechos de las personas con discapacidad,
+        </p>
+      </div>
+    </div>
+
+    <!-- Clasificación/deportes -->
+    <div id="deportes" class="row contenedor-deportes">
+
+      <div class="col-md-12 text-center">
+        <h1 class="title_white">Deportes</h1>
+      </div>
+
+      <div class="col-md-12">
+          <div class="row ">
+              <div class="col-md-6 ">
+                <a href="./accesibilidad.html" class="img-servicio-access text-center">
+                  <h2>Patinaje</h2>
+                </a>
+              </div>
+
+              <div class="col-md-6">
+                <a href="./capacitacion.html" class="img-servicio-access text-center">
+                  <h2>Skateboard</h2>
+                </a>
+              </div>
+          </div>
+          <div class="row separador">
+              <div class="col-md-6">
+                <a href="./imprenta.html" class="img-servicio-access text-center">
+                  <h2>Motociclismo</h2>
+                </a>
+              </div>
+
+              <div class="col-md-6">
+                <a href="./libro.html" class="img-servicio-access text-center">
+                  <h2>Ciclismo</h2>
+                </a>
+              </div>
+            </div>
+      </div>
+    </div>
+
+    <!-- Alianzas -->
+    <div id="alianzas" class="row contenedor-alianzas">
+      <div class="col-md-12">
+        <div class="page-header">
+          <h1>Alianzas</h1>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <p>
+          Discapacitados Visuales IAP, participa desde hace varios años en una serie de foros y equipos de trabajo nacionales e internacionales abordando distintos aspectos del desarrollo de las personas con discapacidad visual; estamos convencidos de que la articulación de acciones entre las distintas organizaciones de y para personas con esta condición de vida, proporcionarán la fuerza necesaria para lograr mayor impacto en nuestros programas y deportes.
+        </p>
+      </div>
+      
+      <div class="col-md-11 col-md-offset-1 content-cita">
+        <h3>
+          Coalición por los Derechos de las Personas con Discapacidad. (COAMEX)
+        </h3>
+        <p>
+          Desde el año 2011 participamos activamente en la Coalición por los Derechos de las Personas con Discapacidad. COAMEX
+          está integrada por seis reconocidas organizaciones de la sociedad civil: CONFE, Libre Acceso, Kadima, Voz Pro Salud Mental,
+          Iluminemos de Azul y nuestra institución.
+
+          El trabajo de COAMEX se centra en la elaboración de un informe alternativo al que presenta el estado mexicano, sobre la imple-
+          mentación de la Convención sobre los Derechos de las Personas con Discapacidad de Naciones Unidas en México. 
+        </p>
+      </div>
+    </div>
+
+    <!-- Donativos -->
+    <div id="donar" class="row contenedor-donativos">
+      <div class="col-md-7 text-center">
+        <h3>¿Quieres sumarte a nuestra causa?</h3>
+      </div>
+      <div class="col-md-5 text-center">
+        <a role="button" href="./donar.html" class="btn btn-primario">Donar</a>
+      </div>
+    </div>
+
+
+
+  </div>
+</section>
+
+
+<footer>
+
+  <div class="footer-superior">
+    <div class="container f-superior">
+      
+      <div class="row">
+        <div class="col-md-6">
+          <h3>Contacto</h3>
+          <a class="contacto-enlace" href="tel: //55194512" aria-label="contáctenos al teléfono +52 55 19 45 12">(55)55194512</a>
+          <br>
+          <a class="contacto-enlace" href="tel: //55383010" aria-label="contáctenos al teléfono +52 55 38 30 10">(55)55383010</a>
+          <br>
+          <br>
+          <a class="contacto-enlace" href="mailto:patriciahernandez.discapacitaodvisualesiap@yahoo.com.mx" aria-label="escribanos al correo e lectrónico patriciahernandez.discapacitaodvisualesiap@yahoo.com.mx">patriciahernandez.discapacitaodvisualesiap@yahoo.com.mx</a>
+        </div>
+        <div class="col-md-6 text-right">
+          <img src="./img/japdf.png" class="img-footer" alt="Junta de Asistencia Privada del Distrito Federal">
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="footer-inferior">
+    <div class="container f-inferior">
+      <div class="row">
+        <div class="col-md-2 col-md-offset-10 text-right">
+
+          <div class="btn-toolbar " role="toolbar" aria-label="Redes sociales">
+            <div class="btn-group btn-social" role="group" aria-label="Facebook">
+              <a href="#face"><i class="fab fa-facebook-f"></i></a>
+            </div>
+            <div class="btn-group btn-social" role="group" aria-label="Twitter">
+              <a href="#face"><i class="fab fa-twitter"></i></a>
+            </div>
+            <div class="btn-group btn-social" role="group" aria-label="Whatsapp">
+              <a href="#face"><i class="fab fa-whatsapp"></i></a>
+            </div>
+            <div class="btn-group btn-social" role="group" aria-label="Instagram">
+              <a href="#face"><i class="fab fa-instagram"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+</footer>
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="common/jquery/jquery-3.3.1.js"></script>
+<script src="common/js/bootstrap.min.js"></script>
+<script src="common/js/main.js"></script>
+</body>
+</html>
